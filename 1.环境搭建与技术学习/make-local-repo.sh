41faaabbeaff74db4c0e5c81d4d2ecf55d4dac73
Robert Darwin <robert.darwin@outlook.com>
2017-05-13 14:13:09 +0800

    make-local-repo.sh: 测试完成

--- 1.环境搭建与技术学习/make-local-repo.sh
+++ 1.环境搭建与技术学习/make-local-repo.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-cocos new -l cpp tohou-game
-git clone git@github.com:ytqqwer/tohou-game.git tohou-game/remote
-cd tohou-game
-rm -r Classes Resource proj.android
-mv -r remote/* remote/.* .  && rmdir remote
+cocos new -l cpp touhou-game
+git clone git@github.com:ytqqwer/Touhou-Game.git touhou-game/remote
+cd touhou-game
+rm -r Classes Resources proj.android
+mv remote/* remote/.* .  && rmdir remote
 
